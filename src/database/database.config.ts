6524b132f23506ad903682e0f9ee8271b0f9283d
infinitymasters2023 <144238563+infinitymasters2023@users.noreply.github.com>
2026-05-09 15:59:23 +0530

    change b y ash

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,15 +1,32 @@
 import * as sql from 'mssql';
 import { ConfigService } from '@nestjs/config';
 
-export const getDbConfig = (configService: ConfigService): sql.config => ({
-    user: configService.get<string>('DB_USER'),
-    password: configService.get<string>('DB_PASS'),
-    database: configService.get<string>('DB_NAME'),
-    server: configService.get<string>('DB_HOST'),
+export const getDbConfig = (
+    configService: ConfigService,
+): sql.config => ({
+    user:
+        configService.get<string>('DB_USER') || 'azure-sa',
+
+    password:
+        configService.get<string>('DB_PASS') ||
+        'ugsf127ghFHSD86dfsDS',
+
+    database:
+        configService.get<string>('DB_NAME') ||
+        'IAPL',
+
+    server:
+        configService.get<string>('DB_HOST') ||
+        '192.168.1.13',
+
     options: {
-        encrypt: false, // change to true if using Az
-        trustServerCertificate: true, // needed for local dev
+        encrypt: false,
+        trustServerCertificate: true,
     },
-    port: Number(configService.get<number>('DB_PORT', 1433)),
+
+    port: Number(
+        configService.get<number>('DB_PORT') || 1433,
+    ),
+
     requestTimeout: 600000,
 });
